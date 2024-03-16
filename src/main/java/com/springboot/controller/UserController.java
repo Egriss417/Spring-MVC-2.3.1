9ce1be7ec863b5d42ffa2cdb5d1a64ca68e634f8
Egriss417 <73225068+Egriss417@users.noreply.github.com>
2024-03-16 12:49:18 +0300

    Update UserController.java

--- src/main/java/com/springboot/controller/UserController.java
+++ src/main/java/com/springboot/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.springboot.controller;
 
-import org.hibernate.mapping.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -10,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import com.springboot.model.User;
 import com.springboot.service.UserService;
 
-import java.util.HashMap;
 
 @Controller
 public class UserController {
